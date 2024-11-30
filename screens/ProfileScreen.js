a173698a85aa3ce4f7df80c5e161bd12b34aebbe
truongnguyen0206 <nguyenvannhattruong2002@gmail.com>
2024-11-30 08:18:34 +0700

    button-Logout

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
-import  ListCourse from '../Component/MyCourse/ListCourse'
-import HeadProfileScreen from '../Component/Profile/HeadProfileScreen'
+import { View, Text, StyleSheet, Image, Button, Alert } from 'react-native';
+import ListCourse from '../Component/MyCourse/ListCourse';
+import HeadProfileScreen from '../Component/Profile/HeadProfileScreen';
 
-const ProfileScreen = ({ navigation,route }) => {
-  const {email} = route.params;
+const ProfileScreen = ({ navigation, route }) => {
+  const { email } = route.params;
+
+  const handleLogout = () => {
+    Alert.alert(
+      "Logout",
+      "Are you sure you want to log out?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel"
+        },
+        {
+          text: "Logout",
+          onPress: () => navigation.replace('LoginSignUp') // Thay 'LoginScreen' bằng tên màn hình đăng nhập của bạn
+        }
+      ]
+    );
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.headerBackground}>
         <Image source={require('../assets/image/profile/hinhNen.png')} style={styles.backgroundImage} />
       </View>
-      <HeadProfileScreen email={email}/>
-      <Text style={{fontSize: 18, fontWeight: 'bold', marginLeft: 20, marginVertical: 10}}>Saved courses</Text>
-      <ListCourse headerText= 'Basic course' Email = {email} />
+      <HeadProfileScreen email={email} />
+      <Text style={{ fontSize: 18, fontWeight: 'bold', marginLeft: 20, marginVertical: 10 }}>Saved courses</Text>
+      <ListCourse headerText="Basic course" Email={email} />
+      <View style={styles.logoutButton}>
+        <Button title="Logout" color="#f44336" onPress={handleLogout} />
+      </View>
     </View>
   );
 };
@@ -29,12 +50,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#00bcd4',
     zIndex: -1,
     borderWidth: 0.1,
-    borderColor: 'black'
+    borderColor: 'black',
   },
   backgroundImage: {
     width: '100%',
     height: '100%',
     resizeMode: 'cover',
+  },
+  logoutButton: {
+    marginRight: 50,
+    marginLeft: 50,
+    zIndex: 1,
   },
 });
 
